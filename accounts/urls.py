0c8d5d919f47fa51d9b3b1d28f3074f08202c110
cooklee <sbbogus@gmail.com>
2021-04-11 21:23:05 +0200

    removed commented code

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,11 +5,9 @@ from accounts import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('', include(router.urls)),
     path('registration', views.RegisterView.as_view(), name='registration'),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
